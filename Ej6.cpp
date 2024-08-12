5149f40dc35e92fb1b1d3a99c96179edf53c5a87
Matias Tosetti <1011641@ucc.edu.ar>
2024-08-12 16:03:20 -0300

    correccion ejercicio 6

--- Ej6.cpp
+++ Ej6.cpp
@@ -12,24 +12,24 @@ Crear un programa que pida al usuario un número entero positivo "n". Luego impl
 using namespace std;
 
 // Función recursiva para contar dígitos pares en posiciones impares
-int contarParesEnPosicionesImpares(int n, int pos ) {
+int contarParesEnPosicionesImpares( unsigned int n,  int tamanionumero ) { //22005
     // Caso base: si n es 0, no hay más dígitos que revisar
     if (n == 0) {
         return 0;
     }
     // Verificar si la posición actual es impar y el dígito es par
     int digitoActual = n % 10;
-    if (pos % 2 != 0 && digitoActual % 2 == 0) {
+    if (tamanionumero % 2 != 0 && digitoActual % 2 == 0) {
         // Incrementar el contador y realizar la llamada recursiva
-        return 1 + contarParesEnPosicionesImpares(n / 10, pos - 1);
+        return 1 + contarParesEnPosicionesImpares(n / 10, tamanionumero - 1);
     } else {
         // Realizar la llamada recursiva sin incrementar el contador
-        return contarParesEnPosicionesImpares(n / 10, pos - 1);
+        return contarParesEnPosicionesImpares(n / 10, tamanionumero - 1);
     }
 }
 
 int main() {
-    int n;
+    unsigned int n;
     int d;
     cout << "Ingrese cantidad de dígitos: ";
     cin >> d;
